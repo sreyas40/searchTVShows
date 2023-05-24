e17b6357103f6bc3d82825b7151ca3931c8545b5
Sreyas Kumar C V <sreyaskumar40@gmail.com>
2023-05-24 22:58:52 +0530

    functionality improvement

--- app.js
+++ app.js
@@ -7,17 +7,27 @@ const showContainer = document.createElement('DIV');
 searchForm.addEventListener('submit', async (e) => {
    e.preventDefault()
    const query = searchForm.elements.query.value;
+
    try {
+      clearResults()
       const config = { params: { q: query } }
       const getTvShows = await axios.get("https://api.tvmaze.com/search/shows", config);
-      makeTvShows(getTvShows.data)
-   } catch(e) {
-      console.log("API fetch failed")
+      makeTvShows(getTvShows.data);
+      console.log("results")
+      console.log(getTvShows.data)
+   } catch (e) {
+      console.log("fetch failed")
       console.log(e)
    }
 })
-
+function clearResults() {
+   const length = resultsContainer.children.length;
+   for (let i = 0; i < length; i++) {
+      resultsContainer.removeChild(resultsContainer.lastElementChild)
+   }
+}
 function makeTvShows(TVshows) {
+
    for (let item of TVshows) {
       const showContainer = document.createElement('DIV');
       showContainer.classList.add("showContainer");
@@ -26,12 +36,18 @@ function makeTvShows(TVshows) {
       const showTitle = document.createElement('H2');
       showTitle.classList.add("showTitle");
       showTitle.append(item.show.name);
-      if (item.show.image.medium) {
-         showBanner.src = item.show.image.medium;
+
+      if (item.show.image !== null) {
+         showBanner.src = item.show.image.original;
       }
+      else {
+         showBanner.src = "images/rollback.svg";
+         showBanner.style.width = "40%";
+      }
+
       resultsContainer.append(showContainer);
       showContainer.append(showBanner);
       showContainer.append(showTitle);
-      
+
    }
 }
